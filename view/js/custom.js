8ff51dd415fb1d0a08c549092998d52e2186d565
Canos13 <canomsergio13@gmail.com>
2022-06-29 21:35:01 -0500

    First commit

---
+++ view/js/custom.js
@@ -0,0 +1,24 @@
+﻿
+(function ($) {
+    "use strict";
+    var mainApp = {
+        main_fun: function () {
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+            window.scrollReveal = new scrollReveal();
+        },
+        initialization: function () {
+            mainApp.main_fun();
+        }
+    }
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+}(jQuery));
